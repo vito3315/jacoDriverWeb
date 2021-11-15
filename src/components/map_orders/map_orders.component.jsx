7e2bf372986c71af9f9ca34c22714516328d5333
vito3315 <marusiaqwerti@yandex.ru>
2021-11-15 22:48:54 +0400

    test

--- src/components/map_orders/map_orders.component.jsx
+++ src/components/map_orders/map_orders.component.jsx
@@ -19,7 +19,7 @@ const queryString = require('query-string');
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -38,7 +38,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 
 import { getMessaging, getToken } from "firebase/messaging";
 
