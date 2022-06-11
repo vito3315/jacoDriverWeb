b97795fc925d11955d2f79afc1fe2657d164ff03
vito3315 <marusiaqwerti@yandex.ru>
2022-06-11 15:13:04 +0400

    test

--- src/stores/config.js
+++ src/stores/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    //urlApi: 'https://jacochef.ru/api/site/driver.php',  
-    urlApi: 'http://151.248.124.144/driver.php',  
+    urlApi: 'https://jacochef.ru/api/site/driver.php',  
+    //urlApi: 'http://151.248.124.144/driver.php',  
 }
